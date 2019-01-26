3308256eb8cb3269741b9e0e2aa6ca282e9504bc
David Song <david.song0802@gmail.com>
2019-01-26 09:33:40 -0500

    added post requests

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const url = require("url");
 const port = parseInt(process.argv[2]);
 const serverId = "p:" + port.toString();
 
-let network = new Map(),
+let network = new Set(),
     storage = new Map();
 
 const handler = (req, res) => {
@@ -19,9 +19,25 @@ const handler = (req, res) => {
   } else if (req.method === "GET" && reqUrl.pathname === "/request") {
   } else if (req.method === "GET" && reqUrl.pathname === "/get") {
   } else if (req.method === "POST" && reqUrl.pathname === "/store") {
-
-    storage.set("", );
+    let body = "";
+    req.on("data", chunk => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      let data = JSON.parse(body);
+      storage.set(data.key, data.value);
+      res.end(`New key added ${data.key}\n`);
+    });
   } else if (req.method === "POST" && reqUrl.pathname === "/connect") {
+    let body = "";
+    req.on("data", chunk => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      let data = JSON.parse(body);
+      network.add(data.endpoint);
+      res.end(`Endpoint added ${data.endpoint}\n`);
+    });
   } else {
     retcode = 404;
     res.writeHead(404, {"Content-Type": "text/plain"});
